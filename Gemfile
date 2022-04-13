aa4af5a2bf2d98d15626e999507076194cbd4f17
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-04-13 20:11:10 -0300

    add validations to wallpaper file

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "pry-byebug", "~> 3.9"
+
+gem "active_storage_validations", "~> 0.9.7"
--- app/models/wallpaper.rb
+++ app/models/wallpaper.rb
@@ -1,7 +1,11 @@
 class Wallpaper < ApplicationRecord
+  has_one_attached :wallpaper
+
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 3 }
-  has_one_attached :wallpaper
+
+  validates :wallpaper, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 3000.kilobytes, message: 'File is too large! maximun allowed: 3MB' }
+  
 
   #before_save :rename_wallpaper_file
 
